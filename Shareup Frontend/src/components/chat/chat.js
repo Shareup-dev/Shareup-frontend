7fe25e8742bf3290e76be4ad9ca7458de86622c9
shareupDev <dev@shareup.qa>
2021-09-14 13:28:24 +0300

    making for azure

--- Shareup Frontend/src/components/chat/chat.js
+++
@@ -1,8 +0,0 @@
-// const url = "http://localhost:8080";
-// let stompClient;
-
-// function connectToChat(userName){
-//     console.log("Connect to chat")
-//     let socket = new SockJS(url + '/chat'/ + userName)
-//     stompClient = Stomp.over(socket);
-// }
