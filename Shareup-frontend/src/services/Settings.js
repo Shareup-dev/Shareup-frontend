daa4e65f98076ae84c3583aa0aaed6423c8a7973
Shareup-dev <dev@shareup.qa>
2021-10-10 14:16:03 +0300

    pulling from remya branch

--- Shareup-frontend/src/services/Settings.js
+++ Shareup-frontend/src/services/Settings.js
@@ -1,18 +1,17 @@
+const settings = {
+  dev: {
+    apiUrl: "http://localhost:8081",
+  },
+  staging: {
+    apiUrl: "http://104.43.220.197:8081",
+  },
+  prod: {
+    apiUrl: "http://backend.shareup.digital",
+  },
+};
 
-    const settings = {
-        dev: {
-          apiUrl: "http://localhost:8081",
-        },
-        staging: {
-          apiUrl: "http://104.43.220.197:8081",
-        },
-        prod: {
-          apiUrl: "http://backend.shareup.digital",
-        },
-      };
-      
-      const getCurrentSettings = () => {
-        return settings.staging;
-      };
-      
-      export default getCurrentSettings();
+const getCurrentSettings = () => {
+  return settings.prod;
+};
+
+export default getCurrentSettings();
