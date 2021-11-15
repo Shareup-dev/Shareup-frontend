ac7dfed29e60207fe5abc255af7e6a39c102ff4c
mohamed1796 <mohamedgalal1796@gmail.com>
2021-11-15 15:26:43 +0300

    backup

---
+++ Shareup-frontend/src/services/Settings.js
@@ -0,0 +1,17 @@
+const settings = {
+  dev: {
+    apiUrl: "http://localhost:8080",
+  },
+  staging: {
+    apiUrl: "http://104.43.220.197:8080",
+  },
+  prod: {
+    apiUrl: "https://shareup.digital/backend",
+  },
+};
+
+const getCurrentSettings = () => {
+  return settings.prod;
+};
+
+export default getCurrentSettings();
