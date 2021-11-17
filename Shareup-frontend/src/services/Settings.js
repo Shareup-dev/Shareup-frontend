1cfbc896eb2eebc83a2c9865c1465f49734a4680
ehabelkatib <ehab_el_khatib@hotmail.com>
2021-11-17 17:23:09 +0300

    size of post popup, guide screen components size

--- Shareup-frontend/src/services/Settings.js
+++ Shareup-frontend/src/services/Settings.js
@@ -3,7 +3,7 @@ const settings = {
     apiUrl: "http://localhost:8080",
   },
   staging: {
-    apiUrl: "http://104.43.220.197:8080",
+    apiUrl: "http:/192.168.100.2:8080",
   },
   prod: {
     apiUrl: "https://shareup.digital/backend",
