e0f5f154fcf8d802bb41f1ae5097c9deb3a0e249
ehabelkatib <ehab_el_khatib@hotmail.com>
2021-12-05 09:26:37 +0300

    reaction updates

--- Shareup-frontend/src/components/post/PostCommentBoxComponent.jsx
+++ Shareup-frontend/src/components/post/PostCommentBoxComponent.jsx
@@ -20,7 +20,7 @@ export default function PostComponentBoxComponent({ post, setRefresh }) {
 
 
 
-  
+
   const { user } = useContext(UserContext)
   const ref = useRef(null);
 
@@ -28,8 +28,8 @@ export default function PostComponentBoxComponent({ post, setRefresh }) {
   const [showEmojis, setShowEmojis] = useState(false)
   const [showGifs, setShowGifs] = useState(false)
 
-  
-                  
+
+
 
 
 
@@ -76,9 +76,9 @@ export default function PostComponentBoxComponent({ post, setRefresh }) {
 
 
 
- 
 
-    
+
+
 
   return (
     post &&
@@ -115,8 +115,8 @@ export default function PostComponentBoxComponent({ post, setRefresh }) {
 
 
 
-<div className="stickers" style={{zIndex:'999'}}>
-            <img src="/assets/images/sticker-svgrepo-com.svg"  style={{height:'19px'}} alt="" onClick={() => setShowSticker(!showSticker)} /></div>
+          <div className="stickers" style={{ zIndex: '999' }}>
+            <img src="/assets/images/sticker-svgrepo-com.svg" style={{ height: '19px' }} alt="" onClick={() => setShowSticker(!showSticker)} /></div>
           {showSticker &&
             <div className="stickers-bunch active">
               <div style={{ height: '326px', overflowX: 'hidden', overflowY: 'scroll' }} >
@@ -125,7 +125,7 @@ export default function PostComponentBoxComponent({ post, setRefresh }) {
                 ) : (
                   <span>No Gif Chosen</span>
                 )}
-                <Stickers/>
+                <Stickers />
                 {/* <PickerGif onSelected={onGiphySelect}  pickerStyle={{ height: "210px" }} /> */}
 
 
@@ -139,12 +139,12 @@ export default function PostComponentBoxComponent({ post, setRefresh }) {
             </div>
           }
 
-       
 
 
 
 
-                  
+
+
 
 
 
@@ -163,7 +163,7 @@ export default function PostComponentBoxComponent({ post, setRefresh }) {
                 {/* <PickerGif onSelected={onGiphySelect}  pickerStyle={{ height: "210px" }} /> */}
 
 
-    
+
 
 
               </div>
@@ -173,13 +173,13 @@ export default function PostComponentBoxComponent({ post, setRefresh }) {
 
 
 
-        
+
 
           <div className="btncmnt">
 
             <button type="button" onClick={() => handlePostingComment(post.id)} style={{ color: 'blue', padding: '1px', }}>
 
-              <img src="/assets/images/photoscomment.svg" alt="" style={{  padding: "3px", marginTop: "8px", borderStyle: "2px solid black" }} />
+              <img src="/assets/images/photoscomment.svg" alt="" style={{ padding: "3px", marginTop: "8px", borderStyle: "2px solid black" }} />
               {/* </div> */}
               {/* <svg class="svg-icon" viewBox="0 0 20 20">
                           <path d="M17.218,2.268L2.477,8.388C2.13,8.535,2.164,9.05,2.542,9.134L9.33,10.67l1.535,6.787c0.083,0.377,0.602,0.415,0.745,0.065l6.123-14.74C17.866,2.46,17.539,2.134,17.218,2.268 M3.92,8.641l11.772-4.89L9.535,9.909L3.92,8.641z M11.358,16.078l-1.268-5.613l6.157-6.157L11.358,16.078z"></path>
