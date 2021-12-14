bff7e3f3d6dbfc2fc3d156ad49d5a16be0074b7e
Najam Rain <najamrain@gmail.com>
2021-12-14 13:13:26 +0300

    story numbering

--- Shareup-frontend/src/services/Settings.js
+++ Shareup-frontend/src/services/Settings.js
@@ -1,11 +1,9 @@
 const settings = {
   dev: {
-    // apiUrl: "http://localhost:8080",
     apiUrl: "http://192.168.100.91:8080",
 
   },
   staging: {
-    // apiUrl: "http://104.43.220.197:8080",
     apiUrl: "http://192.168.100.1:8080",
   },
   prod: {
