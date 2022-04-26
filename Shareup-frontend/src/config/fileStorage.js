8eb759eb143dd3b93cdadabde16197da92e830be
Abderaouf Zerkouk <raouf.zerkouk@gmail.com>
2022-04-26 11:50:05 +0300

    Changing Project Path

--- Shareup-frontend/src/config/fileStorage.js
+++
@@ -1,7 +0,0 @@
-export default {
-  // baseUrl: "https://shareupdigitalspace.fra1.digitaloceanspaces.com",
-  // baseUrl: "http://192.168.100.88/shareup-backend-test/",
-    //  baseUrl: "https://shareup.s3.us-west-2.amazonaws.com/"
-     baseUrl: ""
-
-};
---
+++ src/config/fileStorage.js
@@ -0,0 +1,7 @@
+export default {
+  // baseUrl: "https://shareupdigitalspace.fra1.digitaloceanspaces.com",
+  baseUrl: "http://192.168.100.88/shareup-backend-test/",
+    //  baseUrl: "https://shareup.s3.us-west-2.amazonaws.com/"
+    //  baseUrl: ""
+
+};
